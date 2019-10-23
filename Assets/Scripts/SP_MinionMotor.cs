697dc89b08d9952b5b79a10f9d2e76ac8dcbe71f
Seth <1017464@student.sae.edu.au>
2019-10-23 16:11:42 +1100

    Added turret, minion. Added scripts for minion, turret and bullet.
    Turret script rotates towards mouse position, shoots bullets.
    Minion script looks at turret and moves towards.
    Bullet script moves forward and destroys itself after 10 seconds.

---
+++ Assets/Scripts/SP_MinionMotor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// This enemy will move towards the turret and die when shot.
+/// </summary>
+public class SP_MinionMotor : MonoBehaviour
+{
+    public GameObject turret;
+    public float speed;
+
+    private void Start()
+    {
+        turret = GameObject.Find("Turret");
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, turret.transform.position) > 1.5f)
+        {
+            //Look at turret
+            transform.LookAt(turret.transform.position);
+            //Move
+            transform.position += transform.forward * Time.deltaTime * speed;
+            //Maintain Y position
+            transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+        }
+
+    }
+}
---
+++ Assets/Scripts/SP_TurretController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Rotates towards mouse position and shoots when mouse button clicked
+/// </summary>
+public class SP_TurretController : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject bulletPoint;
+    public float speed;
+
+    void Update()
+    {
+        //rotate towards mouse
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 3000f))
+        {
+            transform.LookAt(new Vector3 (hit.point.x, transform.position.y, hit.point.z));
+        }
+
+        //shoot when clicked
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //instantiate bullet
+            Instantiate(bullet, bulletPoint.transform.position, bulletPoint.transform.rotation);
+        }
+    }
+}
